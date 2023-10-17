0fdbf88f890f371a7030cb7fcadca171cf5066b8
xinlipn <xinlipn@gmail.com>
2023-10-17 11:24:14 -0700

    Convert smoke_solver_ConvBinWinogradRxS_fp16 to gTest

--- test/gtest/smoke_solver_ConvBinWinogradRxS_fp16.cpp
+++ test/gtest/smoke_solver_ConvBinWinogradRxS_fp16.cpp
@@ -54,7 +54,7 @@ void GetArgs(const TestCase& param, std::vector<std::string>& tokens)
         tokens.push_back(*begin++);
 }
 
-class Conv2dFloat : public testing::TestWithParam<std::vector<TestCase>>
+class Conv2dHalf : public testing::TestWithParam<std::vector<TestCase>>
 {
 };
 
@@ -64,8 +64,8 @@ void Run2dDriver(miopenDataType_t prec)
     std::vector<TestCase> params;
     switch(prec)
     {
-    case miopenFloat: params = Conv2dFloat::GetParam(); break;
-    case miopenHalf:
+    case miopenHalf:  params = Conv2dHalf::GetParam(); break;
+    case miopenFloat:
     case miopenBFloat16:
     case miopenInt8:
     case miopenInt8x4:
@@ -73,11 +73,11 @@ void Run2dDriver(miopenDataType_t prec)
     case miopenDouble:
     case miopenFloat8:
     case miopenBFloat8:
-        FAIL() << "miopenHalf, miopenBFloat16, miopenInt8, miopenInt8x4, miopenInt32, "
+        FAIL() << "miopenFloat, miopenBFloat16, miopenInt8, miopenInt8x4, miopenInt32, "
                   "miopenDouble, miopenFloat8, miopenBFloat8 "
-                  "data type not supported by smoke_solver_ConvBinWinograd3x3U test";
+                  "data type not supported by smoke_solver_ConvBinWinogradRxS_fp16 test";
 
-    default: params = Conv2dFloat::GetParam();
+    default: params = Conv2dHalf::GetParam();
     }
 
     for(const auto& test_value : params)
@@ -101,18 +101,19 @@ void Run2dDriver(miopenDataType_t prec)
 bool IsTestSupportedForDevice(const miopen::Handle& handle)
 {
     std::string devName = handle.GetDeviceName();
-    if(devName == "gfx900" || devName == "gfx906" || devName == "gfx908")
+    //F16 is supported for 906 and 906 only, no WrW
+    if(devName == "gfx906")
         return true;
     else
         return false;
 }
 
-TEST_P(Conv2dFloat, FloatTest)
+TEST_P(Conv2dHalf, HalfTest)
 {
     const auto& handle = get_handle();
     if(IsTestSupportedForDevice(handle) && !SkipTest())
     {
-        Run2dDriver(miopenFloat);
+        Run2dDriver(miopenHalf);
     }
     else
     {
@@ -123,7 +124,7 @@ TEST_P(Conv2dFloat, FloatTest)
 std::vector<TestCase> GetTestCases(void)
 {
     std::vector<std::string> env = {"MIOPEN_FIND_MODE=normal",
-                                    "MIOPEN_DEBUG_FIND_ONLY_SOLVER=ConvBinWinograd3x3U"};
+                                    "MIOPEN_DEBUG_FIND_ONLY_SOLVER=ConvBinWinogradRxS"};
 
     std::string vf = " --verbose --disable-backward-data --disable-backward-weights";
     std::string vb = " --verbose --disable-forward --disable-backward-weights";
@@ -131,13 +132,13 @@ std::vector<TestCase> GetTestCases(void)
     const std::vector<TestCase> test_cases = {
         // clang-format off
     //smoke_solver_ConvAsmImplicitGemmV4R1Dynamic
-    TestCase{env, vf + " --input 1 20 20 20 --weights 20 20 3 3 --pads_strides_dilations 1 1 1 1 1 1"},
-    TestCase{env, vb + " --input 1 20 20 20 --weights 20 20 3 3 --pads_strides_dilations 1 1 1 1 1 1"}
+    TestCase{env, vf + " --input 1 40 20 20 --weights 20 40 3 3 --pads_strides_dilations 1 1 1 1 1 1"},
+    TestCase{env, vb + " --input 1 20 20 20 --weights 40 20 3 3 --pads_strides_dilations 1 1 1 1 1 1"}
         // clang-format on
     };
     return test_cases;
 }
 
-INSTANTIATE_TEST_SUITE_P(SmokeSolverConvBinWinograd3x3U,
-                         Conv2dFloat,
+INSTANTIATE_TEST_SUITE_P(SmokeSolverConvBinWinogradRxSFp16,
+                         Conv2dHalf,
                          testing::Values(GetTestCases()));
